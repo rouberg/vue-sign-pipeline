c7c457312db9c97daf3726c66ff376aa2d47a460
rou <rou@Tiny.local>
2018-09-07 19:03:34 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
+  extends: [
+    'plugin:vue/recommended',
+    'standard'
+  ],
   env: {
-    browser: true,
     commonjs: true,
     es6: true,
     amd: true
   },
-  extends: 'standard',
   plugins: [
-    'html'
-  ]
+    'vue'
+  ],
+  rules: {
+    "indent": ["warn", 2, { "SwitchCase": 1 }],
+    "prefer-promise-reject-errors": 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
 }
